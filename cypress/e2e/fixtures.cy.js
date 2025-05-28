722966a97955f9143bdfb223f2bf80c94bc8c819
cypresstraining <cypresstraining7@gmail.com>
2025-05-28 08:05:29 +0530

    commiting to v1

--- cypress/e2e/fixtures.cy.js
+++ cypress/e2e/fixtures.cy.js
@@ -2,3 +2,10 @@
 //I am in version 2 example 
 //I am in version 3 example
 // I am in version 4 example    
+
+// I am in version 5 example
+// I am in version 6 example    
+// I am in version 7 example
+// I am in version 8 example
+// I am in version 9 example
+// I am in version 10 example

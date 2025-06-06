743ee451877fe75012d54ea52ebd2b49eea25704
cypresstraining <cypresstraining7@gmail.com>
2025-06-06 08:28:44 +0530

    crreate a new tes tfile

---
+++ cypress/e2e/totest_github.cy.js
@@ -0,0 +1,6 @@
+describe("Read the data from the CSV file in the for loop", () => {
+    it("Read the data from the CSV file", () => {
+        console.log("Reading the data from the CSV file");
+        cy.log("Reading the data from the CSV file");
+    })
+})
